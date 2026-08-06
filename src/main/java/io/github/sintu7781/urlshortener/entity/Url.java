cb8369a4bebb912850fb8300b52a41c192cc3be8
Sintu Kumar <sintukr7781@gmail.com>
2026-08-07 03:01:53 +0530

    create Url entity with JPA annotations

---
+++ src/main/java/io/github/sintu7781/urlshortener/entity/Url.java
@@ -0,0 +1,40 @@
+package io.github.sintu7781.urlshortener.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(
+        name = "urls",
+        indexes = {
+                @Index(name = "idx_short_code", columnList = "shortCode", unique = true)
+        }
+)
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
+public class Url {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 2048)
+    private String originalUrl;
+
+    @Column(nullable = false, unique = true, length = 10)
+    private String shortCode;
+
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    public void onCreate() {
+        this.createdAt = LocalDateTime.now();
+    }
+}
