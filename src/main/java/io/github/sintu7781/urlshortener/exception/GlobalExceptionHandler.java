2aff9e40051864330385f5effa19f46546994074
Sintu Kumar <sintukr7781@gmail.com>
2026-08-07 04:32:50 +0530

    add GlobalExceptionHandler for handling UrlNotFoundException

---
+++ src/main/java/io/github/sintu7781/urlshortener/exception/GlobalExceptionHandler.java
@@ -0,0 +1,20 @@
+package io.github.sintu7781.urlshortener.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(UrlNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public Map<String, String> handleUrlNotFound(UrlNotFoundException ex) {
+        return Map.of(
+                "message", ex.getMessage()
+        );
+    }
+}
