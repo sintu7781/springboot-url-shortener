5753e777c049247bad54b506df0100191b249c2a
Sintu Kumar <sintukr7781@gmail.com>
2026-08-07 17:50:17 +0530

    add logic to handle existing URLs and validate expiration time in createShortUrl method

--- src/main/java/io/github/sintu7781/urlshortener/service/UrlServiceImpl.java
+++ src/main/java/io/github/sintu7781/urlshortener/service/UrlServiceImpl.java
@@ -3,11 +3,14 @@ package io.github.sintu7781.urlshortener.service;
 import io.github.sintu7781.urlshortener.dto.request.CreateShortUrlRequest;
 import io.github.sintu7781.urlshortener.dto.response.UrlResponse;
 import io.github.sintu7781.urlshortener.entity.Url;
+import io.github.sintu7781.urlshortener.exception.UrlExpiredException;
 import io.github.sintu7781.urlshortener.exception.UrlNotFoundException;
 import io.github.sintu7781.urlshortener.repository.UrlRepository;
 import io.github.sintu7781.urlshortener.util.Base62Generator;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 
 @Service
 @RequiredArgsConstructor
@@ -18,6 +21,27 @@ public class UrlServiceImpl implements UrlService{
     @Override
     public UrlResponse createShortUrl(CreateShortUrlRequest request) {
 
+        Url existing = urlRepository.findByOriginalUrl(request.getUrl())
+                .orElse(null);
+
+        if(existing != null) {
+            return UrlResponse.builder()
+                    .id(existing.getId())
+                    .originalUrl(existing.getOriginalUrl())
+                    .shortCode(existing.getShortCode())
+                    .shortUrl("http://localhost:8080/" + existing.getShortCode())
+                    .clickCount(existing.getClickCount())
+                    .build();
+        }
+
+        if(request.getExpiresAt() != null &&
+        request.getExpiresAt().isBefore(LocalDateTime.now())) {
+
+            throw new IllegalArgumentException(
+                    "Expiration time must be in the future."
+            );
+        }
+
         String shortCode;
 
         do {
@@ -27,6 +51,7 @@ public class UrlServiceImpl implements UrlService{
         Url url = Url.builder()
                 .originalUrl(request.getUrl())
                 .shortCode(shortCode)
+                .expiresAt(request.getExpiresAt())
                 .build();
 
         Url saved = urlRepository.save(url);
@@ -45,6 +70,20 @@ public class UrlServiceImpl implements UrlService{
         Url url = urlRepository.findByShortCode(shortCode)
                 .orElseThrow(() ->
                         new UrlNotFoundException("Short URL not found"));
+
+        if(!url.isActive()) {
+            throw new UrlNotFoundException("Short URL is inactive");
+        }
+
+        if(url.getExpiresAt() != null &&
+        url.getExpiresAt().isBefore(LocalDateTime.now())) {
+
+            throw new UrlExpiredException();
+        }
+
+        url.setClickCount(url.getClickCount() + 1);
+
+        urlRepository.save(url);
 
         return url.getOriginalUrl();
     }
