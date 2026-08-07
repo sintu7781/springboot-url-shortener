3ef59d3f54dd59417e041e0118717e8a2dd6d8c2
Sintu Kumar <sintukr7781@gmail.com>
2026-08-07 17:47:59 +0530

    add default constructor to UrlNotFoundException for consistent error messaging

--- src/main/java/io/github/sintu7781/urlshortener/exception/UrlNotFoundException.java
+++ src/main/java/io/github/sintu7781/urlshortener/exception/UrlNotFoundException.java
@@ -2,6 +2,10 @@ package io.github.sintu7781.urlshortener.exception;
 
 public class UrlNotFoundException extends RuntimeException {
 
+    public UrlNotFoundException() {
+        super("This URL is not found.");
+    }
+
     public UrlNotFoundException(String message) {
         super(message);
     }
