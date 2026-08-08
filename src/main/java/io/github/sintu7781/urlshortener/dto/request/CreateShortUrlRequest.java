b86d300e2e8aebae1238c2dc5609f8c911ee3b91
Sintu Kumar <sintukr7781@gmail.com>
2026-08-08 06:37:03 +0530

    feat: add validation for customAlias field in CreateShortUrlRequest

--- src/main/java/io/github/sintu7781/urlshortener/dto/request/CreateShortUrlRequest.java
+++ src/main/java/io/github/sintu7781/urlshortener/dto/request/CreateShortUrlRequest.java
@@ -1,6 +1,7 @@
 package io.github.sintu7781.urlshortener.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -11,8 +12,14 @@ import java.time.LocalDateTime;
 public class CreateShortUrlRequest {
 
     @NotBlank(message = "URL is required")
-
     private String url;
+
+    @Pattern(
+            regexp = "^[a-zA-Z0-9_-]{3,30}$",
+            message =
+                    "Custom alias must contain 3-30 characters and only letters, number, '_' or '-'."
+    )
+    private String customAlias;
 
     private LocalDateTime expiresAt;
 }
