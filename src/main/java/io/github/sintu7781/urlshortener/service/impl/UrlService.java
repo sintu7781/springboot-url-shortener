d65ae369998bf494394eb02a58fab8d454176b68
Sintu Kumar <sintukr7781@gmail.com>
2026-08-08 05:37:04 +0530

    fix: update package declaration in UrlService to match implementation structure

--- src/main/java/io/github/sintu7781/urlshortener/service/impl/UrlService.java
+++ src/main/java/io/github/sintu7781/urlshortener/service/impl/UrlService.java
@@ -1,4 +1,4 @@
-package io.github.sintu7781.urlshortener.service;
+package io.github.sintu7781.urlshortener.service.impl;
 
 import io.github.sintu7781.urlshortener.dto.request.CreateShortUrlRequest;
 import io.github.sintu7781.urlshortener.dto.response.UrlResponse;
