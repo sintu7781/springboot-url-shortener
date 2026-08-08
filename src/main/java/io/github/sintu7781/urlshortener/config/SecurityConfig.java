df9886f6a198509dee3c2a4b369debdb9c3c68d8
Sintu Kumar <sintukr7781@gmail.com>
2026-08-09 03:05:33 +0530

    refactor: simplify csrf configuration in SecurityConfig

--- src/main/java/io/github/sintu7781/urlshortener/config/SecurityConfig.java
+++ src/main/java/io/github/sintu7781/urlshortener/config/SecurityConfig.java
@@ -4,16 +4,17 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
 
     @Bean
-    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    SecurityFilterChain securityFilterChain(HttpSecurity http) {
 
         http
-                .csrf(csrf -> csrf.disable())
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                         .anyRequest().permitAll()
                 )
