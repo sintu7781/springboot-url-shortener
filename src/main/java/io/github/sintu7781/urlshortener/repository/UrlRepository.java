d53992e738609fe3221be75a53147f7bdd223614
Sintu Kumar <sintukr7781@gmail.com>
2026-08-08 05:38:22 +0530

    feat: add custom query method to retrieve the next ID from the sequence

--- src/main/java/io/github/sintu7781/urlshortener/repository/UrlRepository.java
+++ src/main/java/io/github/sintu7781/urlshortener/repository/UrlRepository.java
@@ -2,6 +2,7 @@ package io.github.sintu7781.urlshortener.repository;
 
 import io.github.sintu7781.urlshortener.entity.Url;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
@@ -12,4 +13,7 @@ public interface UrlRepository extends JpaRepository<Url, Long> {
     Optional<Url> findByOriginalUrl(String originalUrl);
 
     boolean existsByShortCode(String shortCode);
+
+    @Query(value = "SELECT nextval('url_id_seq')", nativeQuery = true)
+    Long getNextId();
 }
