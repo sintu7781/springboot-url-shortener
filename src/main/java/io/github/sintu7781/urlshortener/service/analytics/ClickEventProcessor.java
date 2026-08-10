20c62d71503fcb0c6a8e47ae2913dbed3b31820f
Sintu Kumar <sintukr7781@gmail.com>
2026-08-11 03:10:17 +0530

    feat: implement ClickEventProcessor for handling click events and saving URL click data

---
+++ src/main/java/io/github/sintu7781/urlshortener/service/analytics/ClickEventProcessor.java
@@ -0,0 +1,42 @@
+package io.github.sintu7781.urlshortener.service.analytics;
+
+import io.github.sintu7781.urlshortener.dto.event.ClickEvent;
+import io.github.sintu7781.urlshortener.entity.Url;
+import io.github.sintu7781.urlshortener.entity.UrlClick;
+import io.github.sintu7781.urlshortener.repository.UrlClickRepository;
+import io.github.sintu7781.urlshortener.repository.UrlRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class ClickEventProcessor {
+
+    private final UrlRepository urlRepository;
+
+    private final UrlClickRepository urlClickRepository;
+
+    @Transactional
+    public void process(ClickEvent event) {
+
+        Url url = urlRepository
+                .findByShortCode(event.shortCode())
+                .orElse(null);
+
+        if(url == null) {
+            return;
+        }
+
+        UrlClick click = UrlClick.builder()
+                .url(url)
+                .clickedAt(event.clickedAt())
+                .ipAddress(event.ipAddress())
+                .userAgent(event.userAgent())
+                .referrer(event.referrer())
+                .build();
+
+        urlClickRepository.save(click);
+
+    }
+}
