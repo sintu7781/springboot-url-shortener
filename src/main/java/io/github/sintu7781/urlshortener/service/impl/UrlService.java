fcaa28bc25adad3af287f4f0bb16c1c1df1300ec
Sintu Kumar <sintukr7781@gmail.com>
2026-08-10 05:35:30 +0530

    feat: update getOriginalUrl method to include HttpServletRequest parameter

--- src/main/java/io/github/sintu7781/urlshortener/service/impl/UrlService.java
+++ src/main/java/io/github/sintu7781/urlshortener/service/impl/UrlService.java
@@ -2,10 +2,14 @@ package io.github.sintu7781.urlshortener.service.impl;
 
 import io.github.sintu7781.urlshortener.dto.request.CreateShortUrlRequest;
 import io.github.sintu7781.urlshortener.dto.response.UrlResponse;
+import jakarta.servlet.http.HttpServletRequest;
 
 public interface UrlService {
 
     UrlResponse createShortUrl(CreateShortUrlRequest request);
 
-    String getOriginalUrl(String shortCode);
+    String getOriginalUrl(
+            String shortCode,
+            HttpServletRequest request
+    );
 }
