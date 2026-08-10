cd48480815892d14107e8e2a459f5827614d2924
Sintu Kumar <sintukr7781@gmail.com>
2026-08-10 05:37:06 +0530

    feat: enhance redirect method to utilize HttpServletRequest and improve URL handling

--- src/main/java/io/github/sintu7781/urlshortener/controller/RedirectController.java
+++ src/main/java/io/github/sintu7781/urlshortener/controller/RedirectController.java
@@ -1,12 +1,15 @@
 package io.github.sintu7781.urlshortener.controller;
 
 import io.github.sintu7781.urlshortener.service.impl.UrlService;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
 
 @RestController
 @RequiredArgsConstructor
@@ -16,14 +19,18 @@ public class RedirectController {
 
     @GetMapping("/{shortCode}")
     public ResponseEntity<Void> redirect(
-            @PathVariable String shortCode
+            @PathVariable String shortCode,
+            HttpServletRequest request
     ) {
 
-        String originalUrl = urlService.getOriginalUrl(shortCode);
+        String originalUrl = urlService.getOriginalUrl(
+                shortCode,
+                request
+        );
 
         return ResponseEntity
-                .status(302)
-                .header(HttpHeaders.LOCATION, originalUrl)
+                .status(HttpStatus.FOUND)
+                .location(URI.create(originalUrl))
                 .build();
     }
 }
