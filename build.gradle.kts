d9f35c7b0541bfd8300f9e5a55d78df7f52c6377
Sintu Kumar <sintukr7781@gmail.com>
2026-08-11 08:47:51 +0530

    fix: set user timezone for BootRun task to UTC

--- build.gradle.kts
+++ build.gradle.kts
@@ -53,3 +53,7 @@ tasks.withType<Test> {
     useJUnitPlatform()
     systemProperty("user.timezone", "UTC")
 }
+
+tasks.named<org.springframework.boot.gradle.tasks.run.BootRun>("bootRun") {
+    systemProperty("user.timezone", "UTC")
+}
