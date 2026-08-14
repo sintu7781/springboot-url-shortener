f8243eae74f1c9c2e8e3ec97fd60fdf10067f4ae
Sintu Kumar <sintukr7781@gmail.com>
2026-08-14 06:17:20 +0530

    feat: add LettuceClientOptionsBuilderCustomizer for Redis configuration

--- src/main/java/io/github/sintu7781/urlshortener/config/RedisConfig.java
+++ src/main/java/io/github/sintu7781/urlshortener/config/RedisConfig.java
@@ -1,7 +1,19 @@
 package io.github.sintu7781.urlshortener.config;
 
+import io.lettuce.core.MaintNotificationsConfig;
+import org.springframework.boot.data.redis.autoconfigure.LettuceClientOptionsBuilderCustomizer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RedisConfig {
+
+    @Bean
+    public LettuceClientOptionsBuilderCustomizer lettuceClientOptionsCustomizer() {
+
+        return clientOptionsBuilder ->
+                clientOptionsBuilder.maintNotificationsConfig(
+                MaintNotificationsConfig.disabled()
+        );
+    }
 }
