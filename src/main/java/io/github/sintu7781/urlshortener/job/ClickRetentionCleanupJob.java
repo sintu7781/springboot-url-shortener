d3daa150d9d1dc379ea519e9d5cd6dd34309edb4
Sintu Kumar <sintukr7781@gmail.com>
2026-08-16 02:11:16 +0530

    feat: add ClickRetentionCleanupJob to manage scheduled cleanup of expired clicks

---
+++ src/main/java/io/github/sintu7781/urlshortener/job/ClickRetentionCleanupJob.java
@@ -0,0 +1,31 @@
+package io.github.sintu7781.urlshortener.job;
+
+import io.github.sintu7781.urlshortener.service.analytics.ClickRetentionCleanupService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import net.javacrumbs.shedlock.spring.annotation.SchedulerLock;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class ClickRetentionCleanupJob {
+
+    private final ClickRetentionCleanupService cleanupService;
+
+    @SchedulerLock(
+            name = "clickRetentionCleanup",
+            lockAtMostFor = "PT30M",
+            lockAtLeastFor = "PT10S"
+    )
+    public void cleanExpiredClicks() {
+
+        int deleted =
+                cleanupService.cleanupBatch();
+
+        log.info(
+                "Click retention scheduled job completed. deleted={}",
+                deleted
+        );
+    }
+}
