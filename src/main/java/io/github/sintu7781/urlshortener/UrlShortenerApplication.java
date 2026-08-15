4617d383ad2f257f32a3696b4d7f648ed89b18eb
Sintu Kumar <sintukr7781@gmail.com>
2026-08-16 02:11:07 +0530

    style: format main method for improved readability

--- src/main/java/io/github/sintu7781/urlshortener/UrlShortenerApplication.java
+++ src/main/java/io/github/sintu7781/urlshortener/UrlShortenerApplication.java
@@ -4,12 +4,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@EnableScheduling
+
 @SpringBootApplication
+@EnableScheduling
 public class UrlShortenerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(UrlShortenerApplication.class, args);
+
+        SpringApplication.run(
+                UrlShortenerApplication.class,
+                args
+        );
     }
 
 }
