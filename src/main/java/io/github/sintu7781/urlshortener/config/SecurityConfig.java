947280dbf78e19ce973f4ec35c309b3e71fa0ce4
Sintu Kumar <sintukr7781@gmail.com>
2026-08-19 01:07:58 +0530

    feat: enhance SecurityConfig to restrict actuator metrics access and add admin user details

--- src/main/java/io/github/sintu7781/urlshortener/config/SecurityConfig.java
+++ src/main/java/io/github/sintu7781/urlshortener/config/SecurityConfig.java
@@ -1,10 +1,16 @@
 package io.github.sintu7781.urlshortener.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -23,14 +29,40 @@ public class SecurityConfig {
                         ).permitAll()
 
                         .requestMatchers(
-                                "/actuator/metrics/**",
-                                "/actuator/metrics"
-                        ).authenticated()
+                                "/actuator/metrics",
+                                "/actuator/metrics/**"
+                        ).hasRole("ADMIN")
+
+                        .requestMatchers(
+                                "/api/v1/admin/**"
+                        ).hasRole("ADMIN")
 
                         .anyRequest().permitAll()
                 )
                 .httpBasic(Customizer.withDefaults());
 
         return http.build();
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
+    InMemoryUserDetailsManager userDetailsService(
+            @Value("${app.security.admin.username}") String username,
+            @Value("${app.security.admin.password}") String password
+    ) {
+
+        UserDetails admin =
+                User.builder()
+                        .username(username)
+                        .password(passwordEncoder().encode(password))
+                        .roles("ADMIN")
+                        .build();
+
+        return new InMemoryUserDetailsManager(admin);
     }
 }
